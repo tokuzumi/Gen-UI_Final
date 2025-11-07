4751dd2cfb44193f78e238dde465a97c8e1e5224
[dyad] <daniel.tokuzumi@gmail.com>
2025-11-07 02:25:41 -0300

    [dyad] Limitando a largura do chat para 60% em telas grandes - wrote 1 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { ThreadProvider } from "@/providers/ThreadProvider";
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      {/* Envolver o chat com o Provedor */}
-      <ThreadProvider>
-        {/* Renderiza o wrapper que lida com o carregamento dinâmico no lado do cliente */}
-        <DynamicChatWrapper />
-      </ThreadProvider>
+    <main className="h-screen flex justify-center bg-background">
+      {/* Contêiner que limita a largura: w-full em mobile, lg:w-[60%] em desktop, e altura total */}
+      <div className="w-full lg:w-[60%] h-full">
+        {/* Envolver o chat com o Provedor */}
+        <ThreadProvider>
+          {/* Renderiza o wrapper que lida com o carregamento dinâmico no lado do cliente */}
+          <DynamicChatWrapper />
+        </ThreadProvider>
+      </div>
     </main>
   );
 }
